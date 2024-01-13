255722c2bfdb8dc4f4b5783ff21490ecb0745491
简尚全 <jianshangquan3@gmail.com>
2024-01-13 21:46:47 +0630

    update privacy policies for android

--- src/app/privacy-policies/android/page.js
+++ src/app/privacy-policies/android/page.js
@@ -13,37 +13,36 @@ export default function Android() {
                 <h1 className="font-bold text-[1.5rem]">Privacy Policy for Dhamma app</h1>
                 <div className="pb-5 font-light text-[0.8rem]">Effective Date: 2023-12-01</div>
 
-                <h3 className="font-bold text-[1.3rem]">1. Introduction</h3>
-                <div>Welcome to Dhamma app ("we," "our," or "us"). This Privacy Policy is designed to inform you about the types of information we collect, how we use it, and how we protect your privacy when you use our mobile application, available on the Google Play Store.</div>
+                <h3 className="font-bold text-[1.3rem]">{'1. Introduction'}</h3>
+                <div>{'Welcome to Dhamma app ("we," "our," or "us"). This Privacy Policy is designed to inform you about the types of information we collect, how we use it, and how we protect your privacy when you use our mobile application, available on the Google Play Store.'}</div>
 
 
-                <h3 className="font-bold text-[1.2rem]">2. Information We Collect</h3>
+                <h3 className="font-bold text-[1.2rem]">{'2. Information We Collect'}</h3>
                 <h4 className="font-bold">{'2.1. User-Provided Information:'}</h4>
-                <div>We may collect personal information you provide when using our app, such as [list specific information, e.g., name, email address, etc.].</div>
+                <div>{'We may collect personal information you provide when using our app, such as [list specific information, e.g., name, email address, etc.].'}</div>
 
                 <h4 className="font-bold">{'2.2. Automatically Collected Information:'}</h4>
-                <div>Our app may collect certain information automatically, including [describe types of data, e.g., device information, usage statistics, etc.].</div>
+                <div>{'Our app may collect certain information automatically, including [describe types of data, e.g., device information, usage statistics, etc.].'}</div>
 
                 <h4 className="font-bold">{'2.3. Permissions:'}</h4>
-                <div>The app may request certain permissions on your device to function properly. These permissions may include [list specific permissions required and their purposes].</div>
+                <div>{'The app may request certain permissions on your device to function properly. These permissions may include [list specific permissions required and their purposes].'}</div>
 
 
                 <h3 className="font-bold text-[1.2rem]">{'3. How We Use Your Information'}</h3>
                 <div>
-                    We use the information we collect for various purposes, including but not limited to:
-
-                    Providing and improving our app's features and functionality.
-                    Personalizing your experience.
-                    Analyzing usage patterns to enhance user satisfaction.
+                    {`We use the information we collect for various purposes, including but not limited to:
+    Providing and improving our app's features and functionality.
+    Personalizing your experience.
+    Analyzing usage patterns to enhance user satisfaction.`}
                 </div>
 
 
 
-                <h3 className="font-bold text-[1.2rem]">4. Third-Party Services</h3>
-                <div>Our app may utilize third-party services, and their use of your information is governed by their respective privacy policies. These services may include [list relevant third-party services, e.g., analytics, advertising networks].</div>
+                <h3 className="font-bold text-[1.2rem]">{'4. Third-Party Services'}</h3>
+                <div>{'Our app may utilize third-party services, and their use of your information is governed by their respective privacy policies. These services may include [list relevant third-party services, e.g., analytics, advertising networks].'}</div>
 
                 <h3 className="font-bold text-[1.2rem]">5. Security Measures</h3>
-                <div>We take reasonable measures to protect your information from unauthorized access or disclosure. However, please be aware that no data transmission over the internet is entirely secure.</div>
+                <div>{'We take reasonable measures to protect your information from unauthorized access or disclosure. However, please be aware that no data transmission over the internet is entirely secure.'}</div>
 
                 {/* <h3 className="font-bold text-[1.2rem]">6. Opt-Out Choices</h3>
                 <h4 className="font-bold">a. Opt-Out of Analytics:</h4>
